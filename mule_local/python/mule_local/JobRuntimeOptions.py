6c1038fe017b94a404ab5bf2ddbea06d2515957a
Valentina Schueller <valentina.schueller@gmail.com>
2022-03-29 15:57:29 +0200

    feat: add hyperviscosity fields to runtime options
    necessary for tests

--- mule_local/python/mule_local/JobRuntimeOptions.py
+++ mule_local/python/mule_local/JobRuntimeOptions.py
@@ -93,6 +93,11 @@ class JobRuntimeOptions(InfoError):
         self.libpfasst_use_rexi = None
         self.libpfasst_implicit_coriolis_force = None
         self.libpfasst_use_rk_stepper = None
+        self.libpfasst_u2 = None
+        self.libpfasst_u4 = None
+        self.libpfasst_u6 = None
+        self.libpfasst_u8 = None
+        self.libpfasst_u_fields = None
 
         self.gravitation= None
         self.h0 = None
@@ -595,6 +600,16 @@ class JobRuntimeOptions(InfoError):
             retval += ' --libpfasst-implicit-coriolis-force='+str(self.libpfasst_implicit_coriolis_force)
         if self.libpfasst_use_rk_stepper != None:
             retval += ' --libpfasst-use-rk-stepper='+str(self.libpfasst_use_rk_stepper)
+        if self.libpfasst_u2 != None:
+            retval += ' --libpfasst-u2='+str(self.libpfasst_u2)
+        if self.libpfasst_u4 != None:
+            retval += ' --libpfasst-u4='+str(self.libpfasst_u4)
+        if self.libpfasst_u6 != None:
+            retval += ' --libpfasst-u6='+str(self.libpfasst_u6)
+        if self.libpfasst_u8 != None:
+            retval += ' --libpfasst-u8='+str(self.libpfasst_u8)
+        if self.libpfasst_u_fields != None:
+            retval += ' --libpfasst-u-fields='+str(self.libpfasst_u_fields)
 
         retval += ' --semi-lagrangian-approximate-sphere-geometry='+str(self.semi_lagrangian_approximate_sphere_geometry)
 
